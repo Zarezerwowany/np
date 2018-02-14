a2000dbb9d4f2056f7f5d16cf21fbc25f237f90f
blegocki <36271648+blegocki@users.noreply.github.com>
2018-02-15 00:17:18 +0100

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program by Bartosz: v0.1" << endl;
+    cout << "Kolejna linijka kodu :)" << endl;
+    cout << "Another line of code :) " << endl;
+    return 0;
+}
