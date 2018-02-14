251b094b6f6f88423d6fb51c40dae00dfd20aaca
blegocki <36271648+blegocki@users.noreply.github.com>
2018-02-15 00:25:52 +0100

    Main .cpp wersja 0.6

---
+++ main5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program by Bartosz: v0.1" << endl;
+    cout << "Kolejna linijka kodu :)" << endl;
+    cout << "Another line of code :) " << endl;
+    cout << "Wybierz opcje: "<<endl;
+    cout << "1. Dodawanie"<<endl;
+
+    return 0;
+}
